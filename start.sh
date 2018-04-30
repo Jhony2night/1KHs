14ec2c89e77337060ebcde95659c39768c47d60c
Jhony2night <38860056+Jhony2night@users.noreply.github.com>
2018-05-01 01:04:22 +0200

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+open terminal
+sudo apt-get install firefox
+sudo apt-get install build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake
+firefox https://github.com/JayDDee/cpuminer-opt/archive/v3.8.8.tar.gz
+
+cd Downloads
+
+tar xvzf cpuminer-opt-x.y.z.tar.gz 
+cd cpuminer-opt-x.y.z
+./build.sh
